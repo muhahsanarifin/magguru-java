e8d9e19505ada0b11aa75eb23254479fb75b2cd7
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-11 21:02:38 +0700

    Add TipeDataChar.java

---
+++ fundamental/src/TipeDataChar.java
@@ -0,0 +1,15 @@
+public class TipeDataChar {
+  public static void main(String[] agrs) {
+    char s = 'S';
+    char a = 'A';
+    char n = 'N';
+
+    System.out.print(s);
+    System.out.print(a);
+    System.out.print(n);
+
+    // System.out.println(s);
+    // System.out.println(a);
+    // System.out.println(n);
+  }
+}
