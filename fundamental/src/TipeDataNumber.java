ce830e85a63352c41dc94aab46dc51a076d56d8e
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-11 21:02:51 +0700

    Add TipeDataNumber.java

---
+++ fundamental/src/TipeDataNumber.java
@@ -0,0 +1,21 @@
+public class TipeDataNumber {
+  public static void main(String[] args) {
+    byte iniByte = 100;
+
+    System.out.println(iniByte);
+
+    // short iniShort = 1000;
+    // int iniInt =10000000;
+    // long iniLong = 10000000;
+    // long iniLong2 = 10000000L;
+
+    // float iniFloat = 10.10F;
+    // double iniDouble = 10.10;
+
+    // int decimalInt = 34;
+    // int binaryDecimal = 0b10101010;
+
+
+    // int amount = 1_000_000;
+  }
+}
