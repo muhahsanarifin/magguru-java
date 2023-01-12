1d1a3580e2cba7768357c7ecf441b4826b4a4529
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-12 07:53:21 +0700

    Add OperasiPerbandingan.java

---
+++ fundamental/src/OperasiPerbandingan.java
@@ -0,0 +1,13 @@
+public class OperasiPerbandingan {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 20;
+    // TODO: (==) <- Equel to (!=) <- Not equel to
+    System.out.println(x > y); // False
+    System.out.println(x < y); // True
+    System.out.println(x >= y); // False
+    System.out.println(x <= y); // True
+    System.out.println(x == y); // False
+    System.out.println(x != y); // True
+  }
+}
