a78c13133a8587f96c300545f0048e2041f7b0c3
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-12 21:47:20 +0700

    Updated SwitchStatement.java

--- fundamental/src/SwitchStatement.java
+++ fundamental/src/SwitchStatement.java
@@ -3,40 +3,40 @@ public class SwitchStatement {
     var value = 60;
 
     // TODO: First way
-    // switch(value) {
-    //   case 100:
-    //   System.out.print("Selamat anda lulus denga nilai " + value +". Sangat memuaskan");
-    //   break;
-    //   case 80:
-    //   System.out.print("Selamat anda lulus denga nilai " + value );
-    //   break;
-    //   case 75:
-    //   System.out.print("Selamat anda lulus denga nilai " + value );
-    //   break;
-    //   default:
-    //   System.out.print("Kamu pasti bisa. Jangan pantang menyerah ya");
-    // }
+    switch(value) {
+      case 100:
+      System.out.print("Selamat anda lulus denga nilai " + value +". Sangat memuaskan");
+      break;
+      case 80:
+      System.out.print("Selamat anda lulus denga nilai " + value );
+      break;
+      case 75:
+      System.out.print("Selamat anda lulus denga nilai " + value );
+      break;
+      default:
+      System.out.print("Kamu pasti bisa. Jangan pantang menyerah ya");
+    }
 
     // TODO: Second way
-    // switch(value) {
-    //   case 100 -> System.out.print("Selamat anda lulus denga nilai " + value +". Sangat memuaskan");
-    //   case 80 -> System.out.print("Selamat anda lulus denga nilai " + value );
-    //   case 75 -> System.out.print("Selamat anda lulus denga nilai " + value );
-    //   default -> {System.out.print("Kamu pasti bisa. Jangan pantang menyerah ya");}
-    // }
+    switch(value) {
+      case 100 -> System.out.print("Selamat anda lulus denga nilai " + value +". Sangat memuaskan");
+      case 80 -> System.out.print("Selamat anda lulus denga nilai " + value );
+      case 75 -> System.out.print("Selamat anda lulus denga nilai " + value );
+      default -> {System.out.print("Kamu pasti bisa. Jangan pantang menyerah ya");}
+    }
 
     // TODO: Third way
-    // String resultValue = switch(value) {
-    //   case 100:
-    //   yield "Selamat anda lulus denga nilai " + value +". Sangat memuaskan";
-    //   case 80:
-    //   yield "Selamat anda lulus denga nilai " + value ;
-    //   case 75:
-    //   yield "Selamat anda lulus denga nilai " + value ;
-    //   default:
-    //   yield "Kamu pasti bisa. Jangan pantang menyerah ya";
-    // };
+    String resultValue = switch(value) {
+      case 100:
+      yield "Selamat anda lulus denga nilai " + value +". Sangat memuaskan";
+      case 80:
+      yield "Selamat anda lulus denga nilai " + value ;
+      case 75:
+      yield "Selamat anda lulus denga nilai " + value ;
+      default:
+      yield "Kamu pasti bisa. Jangan pantang menyerah ya";
+    };
 
-    // System.out.print(resultValue);
+    System.out.print(resultValue);
   }
 }
