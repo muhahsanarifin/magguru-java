5468bdfeb5b06d627763cc7065518ce9751a6594
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-13 21:41:23 +0700

    Updated ForLoop.java

--- fundamental/src/ForLoop.java
+++ fundamental/src/ForLoop.java
@@ -1,6 +1,11 @@
 public class ForLoop {
   public static void main(String[] args) {
-    for( var counter = 1; counter <= 10; counter++){ //<- Counter is gonna stop when value counter equel to 10
+    for( var counter = 1; counter <= 10; counter++){ //<- counter is gonna stop when value counter equel to 10
+      System.out.println("Looping " + counter);
+    }
+
+    var counter = 10;
+    for(; counter >= 1; counter--){ //<- counter is gonna stop when value counter equel to 1
       System.out.println("Looping " + counter);
     }
   }
