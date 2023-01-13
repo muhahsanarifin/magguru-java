749aed0cfbaba86fcf542bcaa950bb0dd031cc80
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-01-13 15:30:02 +0700

    Add Forloop.java

---
+++ fundamental/src/ForLoop.java
@@ -0,0 +1,7 @@
+public class ForLoop {
+  public static void main(String[] args) {
+    for( var counter = 1; counter <= 10; counter++){ //<- Counter is gonna stop when value counter equel to 10
+      System.out.println("Looping " + counter);
+    }
+  }
+}
